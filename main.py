bcb3925b8e1ba4386cce665bd876e3c517b4a941
m4um4u1 <m4um4u1@gmail.com>
2023-03-28 19:01:48 +0200

    some text changes

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Microdrip Calculator')
-
-st.write('This app calculates the different variables for a microdrip system')
-
 st.markdown('## Home')
 st.sidebar.markdown('## Home')
+
+st.title('Microdrip Calculator')
+
+st.write('With this app you can calculate different things related to microdrip irrigation or even just want to know how much pressure you need to get a certain flow rate from a tap connected to a barrel')
