3dcdbf38ec48c01ca548641cae34dd4262d192b7
m4um4u1 <m4um4u1@gmail.com>
2023-03-28 15:42:02 +0200

    formatting

--- main.py
+++ main.py
@@ -8,10 +8,3 @@ st.write('This app calculates the different variables for a microdrip system')
 
 st.markdown('## Home')
 st.sidebar.markdown('## Home')
-
-# st.write('Calculate the flow rate in liters per hour')
-# barrel_tap_diameter = st.number_input('Barrel tap diameter (cm)', min_value=0.0, value=0.0)
-# barrel_tap_diameter = barrel_tap_diameter / 100
-# barrel_tap_flow_rate = (barrel_pressure * np.pi * (barrel_tap_diameter**2)) / 4
-
-# st.write('The flow rate is', barrel_tap_flow_rate, 'liters per hour')
--- pages/barrel_volume.py
+++ pages/barrel_volume.py
@@ -6,9 +6,12 @@ st.sidebar.markdown('## Barrel volume')
 
 # Barrel dimensions and volume
 st.write('Calculate the volume of the barrel in liters (when the barrel is full)')
-barrel_width_upper = st.number_input('Barrel width in the top and bottom (cm)', min_value=0.0, value=60.0)
-barrel_width_middel = st.number_input('Barrel width in the middel (cm)', min_value=0.0, value=70.0)
-barrel_height = st.number_input('Barrel height (cm)', min_value=0.0, value=80.0)
+barrel_width_upper = st.number_input(
+    'Barrel width in the top and bottom (cm)', min_value=0.0, value=60.0)
+barrel_width_middel = st.number_input(
+    'Barrel width in the middel (cm)', min_value=0.0, value=70.0)
+barrel_height = st.number_input(
+    'Barrel height (cm)', min_value=0.0, value=80.0)
 
 # Convert to meters
 barrel_width_upper = barrel_width_upper / 100
@@ -16,7 +19,8 @@ barrel_width_middel = barrel_width_middel / 100
 barrel_height = barrel_height / 100
 
 
-# Calculate the barrel volume in liters 
-barrel_volume = (np.pi * barrel_height * ((barrel_width_middel**2)/2 + (barrel_width_upper**2)/4)) / 3 * 1000
+# Calculate the barrel volume in liters
+barrel_volume = (np.pi * barrel_height * ((barrel_width_middel **
+                 2)/2 + (barrel_width_upper**2)/4)) / 3 * 1000
 
 st.write('The volume of the barrel is', barrel_volume, 'liters')
