c5ed00a7bfb9d602025a85f24792cbf133dcdc31
Maurice Hansen <m4um4u1@gmail.com>
2023-03-29 15:10:37 +0200

    some more features and translations

--- pages/barrel_pressure.py
+++ pages/barrel_pressure.py
@@ -2,33 +2,33 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.markdown('## Barrel pressure')
-st.sidebar.markdown('## Barrel pressure')
+st.markdown('## Druck in einem Fass berechnen')
+st.sidebar.markdown('## Druck in einem Fass berechnen')
 
-st.write('Calculate the pressure in the barrel in bar, using water as the working fluid and gravity as the force')
+st.write('Berechne den Druck in einem Fass mit verschiedenen Füllständen und dem Wasserstand über dem Auslauf')
 fluid_density = st.number_input(
-    'Fluid density (kg/m3) - water has a density of 1000 kg/m3', min_value=0.0, value=1000.0)
+    'Dichte der Flüssigkeit (kg/m3) - Wasser hat eine Dichte von 1000 kg/m3', min_value=0.0, value=1000.0)
 barrel_height = st.number_input(
-    'Barrel/waterlevel height (cm)', min_value=0.0, value=90.0)
+    'Wasser/Fasshöhe (cm)', min_value=0.0, value=90.0)
 barrel_tap_height = st.number_input(
-    'Barrel tap height (cm)', min_value=0.0, value=15.0)
+    'Ventilhöhe (cm)', min_value=0.0, value=15.0)
 
 # Calculate from where the pressure is calculated in meters
 calc_height = (barrel_height - barrel_tap_height)
-st.write('The pressure is calculated from',
-         calc_height, 'cm from the top of the barrel')
+st.write('Der Druck wird bis',
+         calc_height, 'cm höhe berechnet')
 barrel_pressure = (fluid_density * 9.81 * (calc_height / 100)) / 100000
 
-st.write('The pressure in the barrel is',
-         barrel_pressure, 'bar when the barrel is full')
+st.write('Der Druck in dem Fass ist',
+         barrel_pressure, 'bar wenn das Fass voll ist')
 
-st.write('Calculate the change in pressure in the barrel in bar with different water levels in height')
+st.write('Hier siehst du die Druckverteilung von einem Fass mit verschiedenen Füllständen und dem Wasserstand über dem Auslauf')
 # Create a dataframe with the hours and the pressure in the barrel in bar with different water levels in height
 
 barrel_water_levels = pd.DataFrame(
-    {'Water level over tap (cm)': np.arange(5, calc_height + 1, 5)})
-barrel_water_levels['Pressure (bar)'] = (
-    fluid_density * 9.81 * (barrel_water_levels['Water level over tap (cm)'] / 100)) / 100000
+    {'Wasserstand über den Auslauf (cm)': np.arange(5, calc_height + 1, 5)})
+barrel_water_levels['Druck (bar)'] = (
+    fluid_density * 9.81 * (barrel_water_levels['Wasserstand über den Auslauf (cm)'] / 100)) / 100000
 
 # reverse the dataframe
 barrel_water_levels = barrel_water_levels.iloc[::-1]
@@ -37,4 +37,4 @@ st.write(barrel_water_levels)
 
 # Create a plot of the pressure in the barrel in bar with different water levels in height
 st.line_chart(data=barrel_water_levels,
-              x='Water level over tap (cm)', y='Pressure (bar)')
+              x='Wasserstand über den Auslauf (cm)', y='Druck (bar)')
--- pages/hose_calculations.py
+++ pages/hose_calculations.py
@@ -23,3 +23,22 @@ pipe_pressure_loss = (0.03 * pipe_length * 1000 * flow_speed) / (pipe_diameter *
 # Convert to Bar
 pipe_pressure_loss = pipe_pressure_loss / 100000
 st.write('Der Druckverlust im Schlauch ist', pipe_pressure_loss, 'Bar')
+
+st.markdown('### Microdrip Lochung')
+st.write('Berechne die Lochung für Microdrip Schläuche')
+
+# Pipe dimensions
+st.write('Gib die Länge vom Schlauch ein und wie viele Löcher pro Meter')
+st.write('Die Länge wird von oben übernommen')
+
+holes_per_meter = st.number_input('Löcher pro Meter', min_value=0.0, value=10.0)
+
+# Calculate the space between the holes in the pipe
+hole_spacing = pipe_length / holes_per_meter
+
+# Convert to cm
+hole_spacing = hole_spacing * 100
+
+st.write('Der Abstand zwischen den Löchern ist', hole_spacing, 'cm')
+
+
