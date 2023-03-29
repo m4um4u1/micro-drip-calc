c252722dd41b2d1229ae5a63aa42815a4efbad60
Maurice Hansen <m4um4u1@gmail.com>
2023-03-29 18:10:05 +0200

    new calculation

--- pages/flow_from_barrel.py
+++ pages/flow_from_barrel.py
@@ -8,7 +8,8 @@ st.write('Berechne die Durchflussmenge mit verschiedenen Messmethoden')
 
 st.markdown('### Volumenstrom')
 # Flow from barrel knowing volume and time to drain
-st.write('Berechne den Volumenstrom/Durchflussmänge aus einem Fass, wenn das Volumen des Fasses und die Zeit bis das Fass leer ist bekannt ist')
+st.write('Der Volumenstrom oder Durchflussrate und Durchflussmenge genannt, gibt an, wie viel Volumen eines Mediums pro Zeitspanne durch einen festgelegten Querschnitt transportiert wird.')
+st.write('Um den Volumenstrom zu berechnen, muss das Volumen des Fasses und die Zeit bis das Fass leer ist bekannt sein.')
 
 barrel_volume = st.number_input('Fassvolumen (l)', min_value=0.0, value=180.0)
 barrel_time_to_drain = st.number_input('Zeit bis das Fass leer ist (min)', min_value=0.0, value=60.0)
@@ -42,3 +43,14 @@ tube_diameter = tube_diameter / 100
 barrel_flow_speed = (barrel_volume_flow_qubic * 4) / (np.power(tube_diameter, 2) * np.pi)
 
 st.write('Die Strömungsgeschwindigkeit aus dem Fass ist', barrel_flow_speed, 'm/s')
+
+st.markdown('### Massenstrom')
+
+st.write('Der Massenstrom, auch Massendurchsatz, ist die Masse eines Mediums, das sich pro Zeitspanne durch einen Querschnitt bewegt. Aus dem Volumenstrom und der Dichte kann der Massenstrom abgeleitet werden.')
+st.write('Der Volumenstrom wird oben berechnet. Die Dichte des Mediums muss hier noch eingegeben werden.')
+tube_density = st.number_input('Dichte des Mediums (kg/m3)', min_value=0.0, value=1000.0)
+
+# Calculate the mass flow from the barrel
+barrel_mass_flow = barrel_volume_flow_qubic * tube_density
+
+st.write('Der Massenstrom aus dem Fass ist', barrel_mass_flow, 'kg/s')
