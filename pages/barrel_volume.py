6f0c8f310a5b547e2cce81c444cf63c909820591
Maurice Hansen <m4um4u1@gmail.com>
2023-03-29 14:44:12 +0200

    übersetzung

--- pages/barrel_volume.py
+++ pages/barrel_volume.py
@@ -1,17 +1,17 @@
 import streamlit as st
 import numpy as np
 
-st.markdown('## Barrel volume')
-st.sidebar.markdown('## Barrel volume')
+st.markdown('## Volumen eines Fasses berechnen')
+st.sidebar.markdown('## Volumen eines Fasses berechnen')
 
 # Barrel dimensions and volume
-st.write('Calculate the volume of the barrel in liters (when the barrel is full)')
+st.write('Gib die Abmessungen des Fasses ein')
 barrel_width_upper = st.number_input(
-    'Barrel width in the top and bottom (cm)', min_value=0.0, value=60.0)
+    'Diameter oben und unten (cm)', min_value=0.0, value=60.0)
 barrel_width_middel = st.number_input(
-    'Barrel width in the middel (cm)', min_value=0.0, value=70.0)
+    'Diameter in der Mitte (cm)', min_value=0.0, value=70.0)
 barrel_height = st.number_input(
-    'Barrel height (cm)', min_value=0.0, value=80.0)
+    'Höhe (cm)', min_value=0.0, value=80.0)
 
 # Convert to meters
 barrel_width_upper = barrel_width_upper / 100
@@ -23,4 +23,4 @@ barrel_height = barrel_height / 100
 barrel_volume = (np.pi * barrel_height * ((barrel_width_middel **
                  2)/2 + (barrel_width_upper**2)/4)) / 3 * 1000
 
-st.write('The volume of the barrel is', barrel_volume, 'liters')
+st.write('Das Volumen des Fasses beträgt ca.', barrel_volume, 'liter')
